d7bea493a5bdc1bc6fa6ec2d657b478740c4f4df
roshanbellary <40147115+roshanbellary@users.noreply.github.com>
2021-02-13 17:10:38 -0800

    Meetings December 2019 Problem 2
    The toxiciest simulation problem you've ever done seen

---
+++ DEC_2019/Meetings.java
@@ -0,0 +1,44 @@
+package DEC_2019;
+//very toxic simulation problem. Currently too sleep deprived and lazy to finish the simulate function right now
+import java.io.*;
+import java.util.*;
+public class Meetings {
+    public static int T;
+    public static int N;
+    public static int L;
+    public static Cow[] l;
+    static class Cow implements Comparable<Cow>{
+        int x,w,v;
+        public Cow(int x, int w, int v){
+            this.x=x;
+            this.w=w;
+            this.v=v;
+        }
+        public int compareTo(Cow y){
+            return Integer.compare(x,y.x);
+        }
+    }
+    public static void simulate(){
+        boolean x=true;
+        while(x){
+            break;
+        }
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader r = new BufferedReader(new FileReader("meetings.in"));
+        StringTokenizer st = new StringTokenizer(r.readLine());
+        N = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
+        l = new Cow[N];
+        int total=0;
+        for (int i=0;i<N;i++){
+            int w = Integer.parseInt(st.nextToken());
+            total+=w;
+            int x = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            l[i]=new Cow(x,w,v);
+        }
+        Arrays.sort(l);
+        
+    }
+}
